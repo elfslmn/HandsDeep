668a07b4b37f0312a90404b5c3b7aa535b820ad5
Elif Salman <elifsalman94@hotmail.com>
2018-03-30 16:24:48 +0300

    copy image not change values to calculate com

--- util.jl
+++ util.jl
@@ -1,9 +1,11 @@
 using ImageView;
+include("transformation.jl");
 
 function showAnnotation(img,joints)
     if typeof(img[1,1]) == Float32
         imgg = convert(Array{Gray{N0f16},3}, img);
     end
+    m = convert(Float32, maximum(imgg));
     img3 = RGB.(imgg);
     w = size(img,2);
     h = size(img,1);
@@ -19,7 +21,50 @@ function showAnnotation(img,joints)
         if yb>h
             yb = h;
         end
-        img3[yt:yb, xr:xl] = RGB{N0f16}(1.,0.,0.);
+        img3[yt:yb, xr:xl] = RGB{N0f16}(m ,0.,0.);
     end
     imshow(img3);
+end
+
+#= Calculate the center of mass
+:param dpt: depth image (float32)
+:return: (x,y,z) center of mass x,y in pixels, z is in mm =#
+function calculateCoM(dpt)
+    min = mmToFloat(10); # hand can be in the range between 10mm to 1500mm
+    max = mmToFloat(1500);
+
+    dc = copy(dpt);
+    dc[dc .< min] = 0.;
+    dc[dc .> max] = 0.;
+
+    num = countnz(dc);
+    x=0.; y=0.; z=0.;
+
+    for c in 1:size(dc,2) # TODO ???????*
+        for r in 1:size(dc,1)
+            w = dc[r,c];
+            if(w > 0)
+                x += c
+                y += r
+                z += w
+            end
+        end
+    end
+    return [x/num, y/num, floatToMm(z/num)]
+end
+
+
+#=  Calculate boundaries, project to 3D, then add offset and backproject to 2D (ux, uy are canceled)
+:com: center of mass, in image coordinates (x,y,z), z in mm
+:size: (x,y,z) extent of the source crop volume in mm
+:param: camera intirinsics
+:return: xstart, xend, ystart, yend, zstart, zend =#
+function comToBounds(com, size, param)
+    zstart = com[3] - size[3] / 2.
+    zend = com[3] + size[3] / 2.
+    xstart = convert(Int64, floor((com[1] * com[3] / param[1] - size[1] / 2.) / com[3]*param[1]))
+    xend = convert(Int64, floor((com[1] * com[3] / param[1] + size[1] / 2.) / com[3]*param[1]))
+    ystart = convert(Int64, floor((com[2] * com[3] / param[2] - size[2] / 2.) / com[3]*param[2]))
+    zstart = convert(Int64, floor((com[2] * com[3] / param[2] + size[2] / 2.) / com[3]*param[2]))
+    return [xstart, xend, ystart, yend, zstart, zend]
 end
