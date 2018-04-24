d1fb57f2c8b872e8e63ed16254cae3df487f0848
Elif Salman <elifsalman94@hotmail.com>
2018-04-24 10:13:42 +0300

    check if image has a hand content

--- NYUreader.jl
+++ NYUreader.jl
@@ -3,41 +3,56 @@ include("util.jl");
 
 searchdir(path,key) = filter(x->contains(x,key), readdir(path));
 searchlabel(path,key) = filter(x->contains(x,key), readdir(path));
-cubeSize = 128;
+imgSize = 128;
 
-function readNYUTesting()
+function readNYUTesting(;sz=-1)
    dir = Pkg.dir(pwd(),"data","NYU", "test");
 
    # read test images
    filenames = searchdir(dir, "png");
-   xtst = Array{Float32, 4}(cubeSize,cubeSize,1,length(filenames));
+   xtst = Array{Float32, 4}(imgSize,imgSize,1,length(filenames));
 
    info("Reading NYU test sequence ...")
-   info("Reading test images...")
+   file = matopen(joinpath(dir,"joint_data.mat"))
+   label = read(file, "joint_xyz")[1,:,:,:]
+   close(file)
+
+   joints3DCrop = Array{Float32, 1}(size(label,2)*size(label,3));
+   ytst = Array{Float32, 2}(size(label,2)*size(label,3), size(label,1));
+   param = getNYUCameraParameters();
+
+   c = 0;
    for i in 1:(length(filenames))
       path = joinpath(dir,filenames[i]);
       raw = rawview(channelview(load(path)));
       dpt = map(Int16, ((2^8).*raw[2,:,:] .+ raw[3,:,:]));
       #preprocess the image, extract hand,normalize depth to [-1,1]
-      p = preprocess(dpt, getNYUCameraParameters(), cubeSize; dset=1);
-      img = reshape(convert(Array{Float32,2}, p), cubeSize,cubeSize,1,1);
-      xtst[:,:,:,i] = img;
-   end
-   info("xtst:", summary(xtst))
+      (p, com) = preprocess(dpt, getNYUCameraParameters(), imgSize; dset=1);
+      if any(isnan, com) || std(p) < 0.5
+          info(:NotInclude , i , filenames[i]);
+          continue;
+      end
 
-   info("Reading test labels...")
-   file = matopen(joinpath(dir,"joint_data.mat"))
-   label = read(file, "joint_uvd")[1,:,:,:]
-   close(file)
-   ytst = Array{Float32, 2}(size(label,2)*size(label,3), size(label,1));
-   for i in 1:size(label,1) #each image
-       for j in 1:size(label,2) #each joint
-           ytst[3*(j-1)+1,i] = label[i,j,1];
-           ytst[3*(j-1)+2,i] = label[i,j,2];
-           ytst[3*(j-1)+3,i] = label[i,j,3];
-       end
+      img = reshape(convert(Array{Float32,2}, p), imgSize,imgSize,1,1);
+      xtst[:,:,:,i] = img;
+
+      com3D = jointImgTo3D(map(Float32,com), param);
+      for j in 1:size(label,2) #each joint
+          joints3DCrop[3*(j-1)+1] = label[i,j,1] - com3D[1];
+          joints3DCrop[3*(j-1)+2] = label[i,j,2] - com3D[2];
+          joints3DCrop[3*(j-1)+3] = label[i,j,3] - com3D[3];
+      end
+      ytst[:,i]= joints3DCrop./300;
+
+      c += 1;
+      if c == sz
+          break;
+      end
    end
-   ytst = ytst ./1000;
+   xtst = xtst[:,:,:,1:c]
+   ytst = ytst[:,1:c]
+
+   info("xtst:", summary(xtst))
    info("ytst:", summary(ytst))
 
    return (xtst, ytst)
@@ -47,37 +62,51 @@ end
 function readNYUTraining(;sz = -1)
     dir = Pkg.dir(pwd(),"data","NYU", "train");
 
-    # read test images
+    # read train images
     filenames = searchdir(dir, "png");
-    xtrn = Array{Float32, 4}(cubeSize,cubeSize,1,length(filenames));
+    xtrn = Array{Float32, 4}(imgSize,imgSize,1,length(filenames));
 
     info("Reading NYU training sequence ...")
-    info("Reading training images...")
+    file = matopen(joinpath(dir,"joint_data.mat"))
+    label = read(file, "joint_xyz")[1,:,:,:]
+    close(file)
+
+    joints3DCrop = Array{Float32, 1}(size(label,2)*size(label,3));
+    ytrn = Array{Float32, 2}(size(label,2)*size(label,3), size(label,1));
+    param = getNYUCameraParameters();
+
+    c = 0;
     for i in 1:(length(filenames))
        path = joinpath(dir,filenames[i]);
        raw = rawview(channelview(load(path)));
        dpt = map(Int16, ((2^8).*raw[2,:,:] .+ raw[3,:,:]));
        #preprocess the image, extract hand,normalize depth to [-1,1]
-       p = preprocess(dpt, getNYUCameraParameters(), cubeSize; dset=1);
-       img = reshape(convert(Array{Float32,2}, p), cubeSize,cubeSize,1,1);
-       xtrn[:,:,:,i] = img;
-    end
-    info("xtrn:", summary(xtrn))
+       (p, com) = preprocess(dpt, getNYUCameraParameters(), imgSize; dset=1);
+       if any(isnan, com) || std(p) < 0.5
+           info(:NotInclude , i , filenames[i]);
+           continue;
+       end
 
-    info("Reading training labels...")
-    file = matopen(joinpath(dir,"joint_data.mat"))
-    label = read(file, "joint_uvd")[1,:,:,:]
-    close(file)
-    label = label[1:size(filenames,1),:,:];
-    ytrn = Array{Float32, 2}(size(label,2)*size(label,3), size(label,1));
-    for i in 1:size(label,1) #each image
-        for j in 1:size(label,2) #each joint
-            ytrn[3*(j-1)+1,i] = label[i,j,1];
-            ytrn[3*(j-1)+2,i] = label[i,j,2];
-            ytrn[3*(j-1)+3,i] = label[i,j,3];
-        end
+       img = reshape(convert(Array{Float32,2}, p), imgSize,imgSize,1,1);
+       xtrn[:,:,:,i] = img;
+
+       com3D = jointImgTo3D(map(Float32,com), param);
+       for j in 1:size(label,2) #each joint
+           joints3DCrop[3*(j-1)+1] = label[i,j,1] - com3D[1];
+           joints3DCrop[3*(j-1)+2] = label[i,j,2] - com3D[2];
+           joints3DCrop[3*(j-1)+3] = label[i,j,3] - com3D[3];
+       end
+       ytrn[:,i]= joints3DCrop./300;
+
+       c += 1;
+       if c == sz
+           break;
+       end
     end
-    ytrn = ytrn ./1000;
+    xtrn = xtrn[:,:,:,1:c]
+    ytrn = ytrn[:,1:c]
+
+    info("xtrn:", summary(xtrn))
     info("ytrn:", summary(ytrn))
 
     return (xtrn, ytrn)
