4473ff6e85f1a944fbc8ce6b7c1b348088fc5f01
Elif Salman <elifsalman94@hotmail.com>
2018-04-26 10:30:46 +0300

    implement refineNet

--- network.jl
+++ network.jl
@@ -61,7 +61,6 @@ function initRefine(inputdim, outputdim, Atype)
     push!(w, xavier(w1,w2,cx1,cy))
     push!(w, zeros(1,1,cy,1))
     x1 = (div(x11-w1+1,2),div(x12-w2+1,2),cy)  # assuming conv4 with p=0, s=1 and pool with p=0,w=s=2
-    info(x1);
 
     #second conv layer (5,5)x8 with (2,2) pooling
     (x21,x22,cx2) =(inputdim[2], inputdim[2], 1) ;
@@ -69,23 +68,21 @@ function initRefine(inputdim, outputdim, Atype)
     push!(w, xavier(w1,w2,cx2,cy))
     push!(w, zeros(1,1,cy,1))
     x2 = (div(x21-w1+1,2),div(x22-w2+1,2),cy)  # assuming conv4 with p=0, s=1 and pool with p=0,w=s=2
-    info(x2);
 
     #third conv layer (3,3)x8 with no pooling
     (x31,x32,cx3) = (inputdim[3], inputdim[3], 1) ;
-    (w1,w2,cy) = (5,5,8);
+    (w1,w2,cy) = (3,3,8);
     push!(w, xavier(w1,w2,cx3,cy))
     push!(w, zeros(1,1,cy,1))
     x3 = (div(x31-w1+1,1),div(x32-w2+1,1),cy)
-    info(x3);
 
-    # 1024 fully connected 1
+    # 512 fully connected
     s = prod(x1) + prod(x2) + prod(x3);
-    push!(w, xavier(1024,s))
-    push!(w, zeros(1024,1))
+    push!(w, xavier(512,s))
+    push!(w, zeros(512,1))
 
-    # last fully connected
-    push!(w, xavier(outputdim,1024))
+    # output layer
+    push!(w, xavier(outputdim,512))
     push!(w, zeros(outputdim,1))
 
     return map(Atype, w)
@@ -246,7 +243,7 @@ function embedNet(w,x; drop = true)
     return x
 end
 
-function scaleNet(w,x_all)
+function scaleNet(w,x_all; drop = true)
     # 1 - full scale (128,128,1)
     #first conv layer (5,5)x8 with (4,4) pooling
     x = conv4(w[1],x_all[1]) .+ w[2]
@@ -306,21 +303,30 @@ function scaleNet(w,x_all)
 end
 
 #TODO complete
-function refineNet(w,x, patchSizes, center)
-    (px, py) = center;
-    s1 = Int64.(patchSizes[1] /2 -0.5);
-    x1 = x[px-s1:px+s1, py-s1:py+s1, :, :];
-    println(summary(x1));
+function refineNet(w,x; drop = true)
+    x64 = x[1];
+    x32 = x[2];
+    x16 = x[3];
+    #largest conv layer (5,5)x8 with (2,2) pooling
+    x64 = conv4(w[1],x64) .+ w[2]
+    x64 = pool(relu.(x64); window =2)
 
-    #first conv layer (5,5)x8 with (2,2) pooling
+    #middle conv layer (5,5)x8 with (2,2) pooling
+    x32 = conv4(w[3],x32) .+ w[4]
+    x32 = pool(relu.(x32); window =2)
 
-    #second conv layer (5,5)x8 with (2,2) pooling
-
-    #third conv layer (3,3)x8 with no pooling
+    #small conv layer (3,3)x8 with no pooling
+    x16 = conv4(w[5],x16) .+ w[6]
+    x16 = relu.(x16);
 
     # 1024 fully connected 1ayer
+    #concanete all scales
+    x = vcat(mat(x64), mat(x32), mat(x16));
+    x = w[7]*mat(x) .+ w[8]
+    x = relu.(x)
 
-    # last fully connected
+    # output layer
+    x = w[9]*mat(x) .+ w[10]
 
     return x
 end
@@ -365,7 +371,7 @@ function huber(a, b; delta = 100)
 end
 
 function huber_loss(w, x, truth, model; param = (241.42, 241.42, 160., 120.))
-    pred = model(w,x);
+    pred = model(w,x,; drop = false);
     r = mean(huber.(pred, truth))
     return r
 end
@@ -376,25 +382,31 @@ function l2reg(w,lambda)
     return J
 end
 
-function objective(w, x, truth, model)
+function objective(w, x, truth, model, l2)
     h = euc_loss(w, x, truth, model);
-    l2 = l2reg(w, 0.001)
+    l2 = l2reg(w, l2)
     return (h+l2)
 end
 
 lossgradient= grad(objective);
 
-function train_sgd(w, dtrn, net, opt) #lr= learning rate, dtrn= all training data
-    for (x,y) in dtrn
-        gr = lossgradient(w,x,y, net)
-        update!(w, gr, opt);
+function train_sgd(w, dtrn, net, opt; l2=0.001)
+    if net == refineNet
+        for (x64,y) in dtrn[1], (x32,y) in dtrn[2], (x16,y) in dtrn[3]
+            gr = lossgradient(w,(x64,x32,x16) ,y, net, l2)
+            update!(w, gr, opt)
+        end
+    else
+        for (x,y) in dtrn
+            gr = lossgradient(w,x,y, net, l2)
+            update!(w, gr, opt);
+        end
     end
 end
 
-
 function accuracy_batch(w, x,y, threshold, net)
     positive =0;
-    pred = net(w,x);
+    pred = net(w,x; drop = false);
     for j in 1:size(pred,2)
         for i in 1:3:size(pred,1)
             dist = sqrt((y[i,j]-pred[i,j])^2 + (y[i+1,j]-pred[i+1,j])^2 + (y[i+2,j]-pred[i+2,j])^2);
@@ -428,15 +440,16 @@ end
 
 #return avarage joint distance error(in mm) and accuracy
 # y should be in 3d world coordinates and normalized(./250)
-function getLossandAccuracy(w,data,threshold, net, param)
+function getLossandAccuracy(w,data,threshold, net; dset = 0)
     dist = 0;
     positive =0;
     all = data.length;
     for (x,y) in data
-        pred = net(w,x);
+        pred = net(w,x; drop = false);
 
-        pred3D = pred.*250;
-        y3D = y.*250;
+        scale = dset == 0 ? 250:300;
+        pred3D = pred.*scale;
+        y3D = y.*scale;
 
         for j in 1:size(pred,2)
             pass = true;
@@ -454,4 +467,18 @@ function getLossandAccuracy(w,data,threshold, net, param)
 
     end
     return (dist/all , positive/all*100 )
+end
+
+function getRefineLoss(w,data)
+    dist = 0;
+    all = data[1].length; print("all=",all);
+    for (x64,y) in data[1], (x32,y) in data[2], (x16,y) in data[3]
+        pred = refineNet(w,(x64,x32,x16); drop = false);
+
+        for j in 1:size(pred,2)
+            d = sqrt((y[1,j]-pred[1,j])^2 + (y[2,j]-pred[2,j])^2 + (y[3,j]-pred[3,j])^2);
+            dist += d; # to calculate mean distance of all joints
+        end
+    end
+    return (dist/all)
 end
